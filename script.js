f9a97cc42a0a45ee92fda127494c2094163eed1d
Aditya Raj <157242162+AdityaR0@users.noreply.github.com>
2025-03-08 13:54:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -183,6 +183,9 @@ function scrollToTop() {
 
 
 
+if (window.location.pathname.endsWith(".html")) {
+    window.history.replaceState(null, "", window.location.pathname.replace(".html", ""));
+}
 
 
 
